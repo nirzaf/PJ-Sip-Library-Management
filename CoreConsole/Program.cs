a7c1daa7ba54fec56b4ef0fc301a5355470de615
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-02-28 11:15:35 +0530

    Add project files.

---
+++ CoreConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Wisdom PJ Sip Library");
+            Console.ReadLine();
+        }
+    }
+}
